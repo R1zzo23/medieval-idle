1dcd55b6719f767e293e241e5c10074586cbdf53
Adam Rizzo <arizzo.dev@gmail.com>
2022-11-26 06:45:53 -0500

    have working buttons, buy building and game engine that runs every second

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var cookies = 0;
+var buildings = 0;
 
 function cookieClick(number) {
-    cookies = cookies + number;
+    cookies += number;
     document.getElementById("cookieCount").innerHTML = cookies;
 }
+
+function buyBuilding(){
+    var buildingCost = Math.floor(10 * Math.pow(1.1,buildings));       //works out the cost of this cursor
+    if(cookies >= buildingCost){                                     //checks that the player can afford the cursor
+        buildings++;                                              //increases number of cursors
+    	cookies -= buildingCost;                                     //removes the cookies spent
+        document.getElementById('buildingCount').innerHTML = buildings;  //updates the number of cursors for the user
+        document.getElementById('cookieCount').innerHTML = cookies;      //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,buildings));           //works out the cost of the next cursor
+    document.getElementById('buildingCost').innerHTML = nextCost;      //updates the cursor cost for the user
+}
+
+window.setInterval(function(){
+    cookieClick(buildings);
+}, 1000);
